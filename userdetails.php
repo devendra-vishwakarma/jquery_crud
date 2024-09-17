2fe89aa15903369c46bfa0c5f918de6fdffc7b38
devendra-vishwakarma <devendravishvakarma128@gmail.com>
2024-09-17 15:49:48 +0530

    jquerycrud

---
+++ userdetails.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table id="tableId">
+        <thead>
+            <th>S.no</th>
+            <th>first</th>
+            <th>second</th>
+            <th>third</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>A</td>
+                <td>B</td>
+                <td>C</td>
+            </tr>
+            <tr>
+                <td>D</td>
+                <td>E</td>
+                <td>F</td>
+            </tr>
+            <tr>
+                <td>I</td>
+                <td>J</td>
+                <td>K</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
